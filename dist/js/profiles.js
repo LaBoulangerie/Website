1e14ad76c327a559b9e4fc420f4da19dcc043cf1
PainOchoco <matteo.planchet@gmail.com>
2022-06-17 00:47:45 +0200

    🙌 Added new team: Julien, BeubzyTv & Narrateur!

--- dist/js/profiles.js
+++ dist/js/profiles.js
@@ -1,55 +1,23 @@
 const profiles = [
-    { uuid: "3ee833a1ce204057a5e2a85212ce8bbe", role: "admin", group: "staff" },
-    { uuid: "e6ad501cbcd746d09e0fee6821ac6053", role: "admin", group: "staff" },
-    { uuid: "afb8df8b1bad46f5826251809d8feca3", role: "mod", group: "staff" },
-    {
-        uuid: "566b4d0347674e888d08f1902ef6940e",
-        role: "builder",
-        group: "staff",
-    },
-    {
-        uuid: "182cce6591ff4043a5934e7c9f13ea15",
-        role: "builder",
-        group: "staff",
-    },
-    { uuid: "58a6efe528de47a3bcf42752ae9f9f1e", role: "dev", group: "staff" },
-    { uuid: "2154818603694e49bf41a5286638e66b", role: "dev", group: "staff" },
-    { uuid: "b8ef1c7615e04b958d474ca133561f5a", role: "dev", group: "staff" },
-    {
-        uuid: "143ed58be4ba4d05b1d0e23183a18654",
-        role: "donor",
-        group: "donors",
-    },
-    {
-        uuid: "9d9b239d920140a996d966db495c7081",
-        role: "donor",
-        group: "donors",
-    },
-    {
-        uuid: "dd99223c3a7e4dfd912f00cdc8eec1aa",
-        role: "donor",
-        group: "donors",
-    },
-    {
-        uuid: "1247c3c7610e44028fe2f509ff74ddc6",
-        role: "donor",
-        group: "donors",
-    },
-    {
-        uuid: "ce96aa7b63d349ff9b4c4362f274f5b7",
-        role: "donor",
-        group: "donors",
-    },
-    {
-        uuid: "bb6dbc9d7f4b412a9061a9b3dbd57803",
-        role: "donor",
-        group: "donors",
-    },
-    {
-        uuid: "1c73a8c901b24a04912ec4c37f53a86c",
-        role: "donor",
-        group: "donors",
-    },
+    { uuid: "3ee833a1ce204057a5e2a85212ce8bbe", role: "admin", group: "staff" }, // PainOchoco
+    { uuid: "e6ad501cbcd746d09e0fee6821ac6053", role: "admin", group: "staff" }, // Briocheboy
+    { uuid: "afb8df8b1bad46f5826251809d8feca3", role: "mod", group: "staff" }, // ZeMarshadow
+    { uuid: "6bf95d89fc414627b833c3dd5bfd45e3", role: "builder", group: "staff" }, // BeubzyTv
+    { uuid: "182cce6591ff4043a5934e7c9f13ea15", role: "builder", group: "staff" }, // Freezipizy
+    { uuid: "566b4d0347674e888d08f1902ef6940e", role: "builder", group: "staff" }, // MrTrynity
+    { uuid: "58a6efe528de47a3bcf42752ae9f9f1e", role: "dev", group: "staff" }, // Grooble_
+    { uuid: "2154818603694e49bf41a5286638e66b", role: "dev", group: "staff" }, // Pikachuz3
+    { uuid: "b8ef1c7615e04b958d474ca133561f5a", role: "dev", group: "staff" }, // Blackoutburst
+    { uuid: "e19cdd65ee50481889d8eb3afb01d248", role: "dev", group: "staff" }, // You_Kill (julien)
+    { uuid: "38f41df9bd5d4061b23a209f004ddea3", role: "scenariste", group: "staff" }, // Nocta_Ligne (Narrateur)
+
+    { uuid: "143ed58be4ba4d05b1d0e23183a18654", role: "donor", group: "donors" }, // Ptah
+    { uuid: "9d9b239d920140a996d966db495c7081", role: "donor", group: "donors" }, // xiviogaming
+    { uuid: "dd99223c3a7e4dfd912f00cdc8eec1aa", role: "donor", group: "donors" }, // Azuyka
+    { uuid: "1247c3c7610e44028fe2f509ff74ddc6", role: "donor", group: "donors" }, // Fryzhen
+    { uuid: "ce96aa7b63d349ff9b4c4362f274f5b7", role: "donor", group: "donors" }, // hiroskiller
+    { uuid: "bb6dbc9d7f4b412a9061a9b3dbd57803", role: "donor", group: "donors" }, // Bossou
+    { uuid: "1c73a8c901b24a04912ec4c37f53a86c", role: "donor", group: "donors" }, // YoutubePoulpe
 ];
 
 export { profiles };
