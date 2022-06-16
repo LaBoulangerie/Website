cb572675ce17ed658493662bbdf98231c0c12f16
PainOchoco <matteo.planchet@gmail.com>
2022-06-17 00:47:00 +0200

    Disabled stats requests momentarily

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,13 +2,13 @@ import { getPlayersSize, getTownsSize, getNationsSize } from "./apiRequests.js";
 import { generateProfile } from "./misc.js";
 import { profiles } from "./profiles.js";
 
-const playersEl = document.getElementById("players");
-const townsEl = document.getElementById("towns");
-const nationsEl = document.getElementById("nations");
+// const playersEl = document.getElementById("players");
+// const townsEl = document.getElementById("towns");
+// const nationsEl = document.getElementById("nations");
 
-playersEl.textContent = await getPlayersSize();
-townsEl.textContent = await getTownsSize();
-nationsEl.textContent = await getNationsSize();
+// playersEl.textContent = await getPlayersSize();
+// townsEl.textContent = await getTownsSize();
+// nationsEl.textContent = await getNationsSize();
 
 for (const profil of profiles) {
     document
