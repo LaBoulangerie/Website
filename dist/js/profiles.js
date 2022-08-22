5fafa718922fe031711e9a111bb865a6b2fd5285
GroobleDierne <grooble.dierne@gmail.com>
2022-08-22 23:59:48 +0200

    Updated staff members + Hyelbi - BlackoutBurst, Beubzy & You_Kill

--- dist/js/profiles.js
+++ dist/js/profiles.js
@@ -2,13 +2,11 @@ const profiles = [
     { uuid: "3ee833a1ce204057a5e2a85212ce8bbe", role: "admin", group: "staff" }, // PainOchoco
     { uuid: "e6ad501cbcd746d09e0fee6821ac6053", role: "admin", group: "staff" }, // Briocheboy
     { uuid: "afb8df8b1bad46f5826251809d8feca3", role: "mod", group: "staff" }, // ZeMarshadow
-    { uuid: "6bf95d89fc414627b833c3dd5bfd45e3", role: "builder", group: "staff" }, // BeubzyTv
+    { uuid: "19a9b1b7790244628848850373c53c8c", role: "builder", group: "staff" }, // Hyelbi
     { uuid: "182cce6591ff4043a5934e7c9f13ea15", role: "builder", group: "staff" }, // Freezipizy
     { uuid: "566b4d0347674e888d08f1902ef6940e", role: "builder", group: "staff" }, // MrTrynity
     { uuid: "58a6efe528de47a3bcf42752ae9f9f1e", role: "dev", group: "staff" }, // Grooble_
     { uuid: "2154818603694e49bf41a5286638e66b", role: "dev", group: "staff" }, // Pikachuz3
-    { uuid: "b8ef1c7615e04b958d474ca133561f5a", role: "dev", group: "staff" }, // Blackoutburst
-    { uuid: "e19cdd65ee50481889d8eb3afb01d248", role: "dev", group: "staff" }, // You_Kill (julien)
     { uuid: "38f41df9bd5d4061b23a209f004ddea3", role: "scenariste", group: "staff" }, // Nocta_Ligne (Narrateur)
 
     { uuid: "143ed58be4ba4d05b1d0e23183a18654", role: "donor", group: "donors" }, // Ptah
